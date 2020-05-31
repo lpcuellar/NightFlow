3b2b73ff64c6f2ee11aebfda3b5f548ae1a7faa5
JoeBlock99 <40869458+JoeBlock99@users.noreply.github.com>
2020-05-31 12:55:54 -0600

    places

--- nightflow/src/app/app.module.ts
+++ nightflow/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { environment } from 'src/environments/environment';
 import { SendEmailComponent } from './auth/send-email/send-email.component';
 import { SearchComponent } from './search/search.component';
 import { ProfileComponent } from './profile/profile.component';
+import { PlaceComponent } from './place/place.component';
 
 
 
@@ -24,6 +25,7 @@ import { ProfileComponent } from './profile/profile.component';
     SendEmailComponent,
     SearchComponent,
     ProfileComponent,
+    PlaceComponent,
   ],
   imports: [
     BrowserModule,
